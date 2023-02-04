563904d5f4535babb314ff42076afc87aedf13d3
FahadZulfi14 <122536758+FahadZulfi14@users.noreply.github.com>
2023-02-04 05:29:15 +0500

    Add files via upload
    my assignment chapter 5 to 9.

---
+++ Js Chepter 5/app.js
@@ -0,0 +1,92 @@
+// -----------------------    chepter no 6-9  -------
+// -------------------------MATH EXPRESSIONS  ---------
+
+
+// Question no 1
+// document.write("<b> Result </b><br />");
+// var a = 10;
+// var total;
+// document.write("<b> the value of a is = </b> " + a);
+
+
+// a = ++a;
+// document.write("<br /><br /><br /><b> the value of  ++a is:11 </b><br />");
+// document.write("<b> Now the value of a is = </b> " + a);
+
+// a = a++;
+// document.write("<br /><br /><br /><b> the value of  a++ is:11 </b><br />");
+// document.write("<b> Now the value of a is = </b> " + 12);
+
+// a = --a;
+// document.write("<br /><br /><br /><b> the value of  --a is:11 </b><br />");
+// document.write("<b> Now the value of a is = </b> " + 11);
+
+// a = a--;
+// document.write("<br /><br /><br /><b> the value of  --a is:11 </b><br />");
+// document.write("<b> Now the value of a is = </b> " + a);
+
+
+
+// Question no 2
+// var a =2;
+// var b =1;
+// document.write("<b> a is : </b>" + a);
+// document.write("<br /><b> b is : </b>" + b);
+// var result = --a - --b + ++b + b--;
+// document.write("<br /><b> Result is : </b>" + result);
+
+
+
+// Question no 3
+// var name = prompt("Enter Your Name");
+// document.write("<h1>" + "Welcome  " + name + "</h1>");
+
+
+
+// Question no 5
+
+// var num = +prompt("Enter Number","5");
+// document.write("You select 1st Number is:  " + num);
+
+// var num2 = prompt("Enter Second Number");
+// document.write("<br/> You select 2nd Number is:  " + num2);
+
+// var result=num*num2;
+// document.write("<br/> Your Result is: "+ result);
+
+
+
+
+// Question no 6
+// document.write("<h4>Subjec  TotalMarks  ObtainMarks  Percentage<h4>");
+// var subject1="English";
+// var subject2="Urdu";
+// var subject3="Math";
+// var englishMarks= +prompt("Enter English Marks");
+// var urduMarks=+prompt("Enter Urdu Marks");
+// var mathMarks=+prompt("Enter Math Marks");
+// var totalMarks= 100;
+// var percentEng = englishMarks/totalMarks*100;
+// var percentUrdu = urduMarks/totalMarks*100;
+// var percentMath = mathMarks/totalMarks*100;
+
+// document.write("English 100      "  + englishMarks  +  "  ==>  %  "   + percentEng);
+// document.write("<br/>Urdu 100    "  + urduMarks    +   " ==>  % "   + percentUrdu);
+// document.write("<br/>Math 100    "  + mathMarks    +   " ==>  %   "   + percentMath);
+
+// var fullMarks=englishMarks+urduMarks+mathMarks;
+// var overAllPercentage=percentEng+percentUrdu+percentMath;
+// document.write("<br/> Total Marks : "    + fullMarks);
+// document.write("<br/> Total Percentage :  % "    + overAllPercentage);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Js Chepter 6-9/app.js
@@ -0,0 +1,196 @@
+// -----------------------    chepter no 5  -------
+// -------------------------MATH EXPRESSIONS  ---------
+
+
+// Question no 1
+// var num = 5;
+// num = 5+3;
+// document.write(num);
+
+
+
+// ---------------------- QUESTION NO 2 
+//================ FOR SUBTRACTION*
+// var num = 5;
+// num = 5-3;
+// document.write(num);
+
+// ================MULTIPLICATION*
+// var num = 5;
+// num = 5-3;
+// document.write(num);
+
+// ================FOR division*
+// var num = 3;
+// num = 3/5;
+// document.write(num);
+
+
+// ================FOR modulus*
+// var whatsLeftOver = 5 % 3;
+// alert(whatsLeftOver);
+
+
+
+// ---------------------- QUESTION NO 3----------
+// var num;
+// num=5+1;
+
+// num=num+7;
+// num=num-1;
+// num=num % 3;
+
+// document.write(num);
+
+
+
+// ---------------------- QUESTION NO 4----------
+// var ticket = 600;
+// var total = 600*5;
+// document.write("Total cost to buy 5 tickets to a movie is Rs" + total);
+
+
+
+// ---------------------- QUESTION NO 5----------
+// var num = 4;
+// num= 4*2;
+// num= 4*3;
+// num= 4*4;
+// num= 4*5;
+// num= 4*6;
+// num= 4*7;
+// num= 4*8;
+// num= 4*9;
+// num= 4*10;
+// document.write(num);
+
+
+
+// ---------------------- QUESTION NO 6----------
+
+// var Celsius=25;
+// var convertFahrenheit = 25*9/5+32
+// document.write(convertFahrenheit + "=°F");
+
+
+// var Fahrenheit=70;
+// var convertCelsius = (70-32)*5/9;
+// document.write(convertCelsius + " °C = 70°F");
+
+
+
+// ---------------------- QUESTION NO 7----------
+// var item1=650;
+// var item2=100;
+
+// var qtyItem1=item1*3;
+// var qtyItem2 = item2*7;
+// var shipingCharges = 100;
+
+// var total = qtyItem1 + qtyItem2 + shipingCharges;
+
+// document.write(qtyItem1);
+
+// document.write(qtyItem2);
+
+// document.write(total );
+
+
+
+
+// ---------------------- QUESTION NO 8----------
+// var marks=980;
+// document.write("TotalMarks =  "+ marks );
+// var marksObtain=804;
+// document.write(" <br>Marks Obtain = " + marksObtain );
+// var percentage=marksObtain/marks*100;
+// document.write("<br> percentage = " + percentage)
+
+
+
+// ---------------------- QUESTION NO 9----------
+// document.write("<h1> Currency in PKR </h1> <br />");
+// var dollars=104.84*10;
+// var saudiRial=28*25;
+// var totalPakRupees=dollars+ saudiRial;
+// document.write("<b> Currency in PKR </b> " + totalPakRupees );
+
+
+
+
+// ---------------------- QUESTION NO 10----------
+// var num= 2;
+// num = num*10/2;
+// document.write(num);
+
+
+
+// ---------------------- QUESTION NO 11----------
+// document.write("<h1> Age Calculator: </h1> <br />");
+
+// var currentYear = 2016;
+// document.write(" Current Year : " + currentYear);
+
+// var birthYear = 1992;
+// document.write("<br />  Birth Year : " + currentYear);
+// var age = currentYear-birthYear;
+// document.write(" <br /> <b> Your Age is  : </b>" + age);
+
+
+
+// ---------------------- QUESTION NO 12----------
+// document.write("<h1> The Geometrizer:</h1> <br />");
+// var radiosCircle = 20;
+// document.write("<b> Radios of a circle = </b> " + radiosCircle);
+// var circumference = 125.6;
+// document.write("<br /><b> The circumference is = </b> " + circumference);
+// var area = 3.14*20**2;
+// document.write(" <br /> Total Area is  : </b>" + area);
+
+
+
+
+
+
+
+
+// ---------------------- QUESTION NO 13----------
+// document.write("<h1> The Lifetime Supply Calculator:</h1> <br />");
+// var favSnack = "chocolate";
+// document.write("<b> favorite snack is = </b> " + favSnack);
+
+
+// var currentAge = 25;
+// document.write("<br /> <b>Current age is = </b> " + currentAge);
+
+
+// var maxAge = 50;
+// document.write("<br /> <b>Estimated Max age is = </b> " + maxAge);
+
+// var perDayAmount = 100;
+// document.write("<br /><b> Amount of snack per day = </b> " + perDayAmount);
+// var cost= (perDayAmount*365);
+// document.write("<br /><b> Amount of snack for ONE year = </b> " + cost);
+// var lifeAmount=cost*25;
+// document.write("<br /><b> you will need total  = </b> " + lifeAmount + " Rupees amount chocolate ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
